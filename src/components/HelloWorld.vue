74f98724442a998b0ab7f471a74d9fcc2bf9e7b2
GeT_Left <martin_lang@163.com>
2022-05-17 04:02:10 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,109 +1,58 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
+    今天 [{{format_date(today_date)}}] - {{pool_name}} 限定池结束 [{{format_date(pool_date)}}]
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+
+    <p>
+      历时 {{months}}月 {{weeks}}周 {{days}}天
+    </p>
+
+    <h3> </h3>
+
+    <div>
+      <el-checkbox v-model="is_fuchao_new" label="去年没有参与覆潮之下活动" size="default" @change='compute()'/>
+      <el-checkbox v-model="is_duosuo_new" label="去年没有参与多索雷斯活动" size="default" @change='compute()' />
+    </div>
+
+    <!-- Element+ -->
+    <div>
+      <el-checkbox v-model="is_monthly_card" label="月卡玩家" size="default" @change='compute()' />
+      <el-checkbox v-model="is_big_monthly_card" label="大月卡玩家" size="default" @change='compute()' />
+
+    </div>
+
+    <!-- 礼包，按每抽价格从低到高 参考：https://ngabbs.com/read.php?tid=31618594 -->
+    <div> 
+      <el-checkbox-group v-model="checked" @change="handleChecked">
+        <el-checkbox v-for="(item, index) in purchase_items" :label="index" :key="index">{{item.label}}</el-checkbox>
+      </el-checkbox-group> 
+       <!-- 其余礼包性价比急剧降低，不推荐 -->
+    </div>
+
+
+    <p>
+      {{ cmsg }}
+    </p>
+    <div id='result' style="width: 600px; margin: auto">
+    <Details :details="[['title 1', details], ['title 2', 'content 2']]" />
+    </div>
+  </div>  
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
+// import { Options, Vue } from "vue-class-component";
+import { Component, Vue } from 'vue-property-decorator'
 
-@Options({
-  props: {
-    msg: String,
-  },
-})
+// @Options({
+//   props: {
+//     msg: String,
+//   },
+// })
+@Component
 export default class HelloWorld extends Vue {
-  msg!: string;
+  
 }
 </script>
 
