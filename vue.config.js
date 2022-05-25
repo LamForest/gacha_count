4288ae8cba74d18490ff3d4b82ba210a22644a6e
GeT_Left <martin_lang@163.com>
2022-05-26 00:02:34 +0800

    deploy to vercel时不能激活webpack-bundle-analyzer 否则报错

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-.BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+// .BundleAnalyzerPlugin;
 module.exports = {
   // ...
   configureWebpack: {
@@ -13,7 +13,7 @@ module.exports = {
       Components({
         resolvers: [ElementPlusResolver()],
       }),
-      new BundleAnalyzerPlugin(),
+      // new BundleAnalyzerPlugin(),
 
     ],
     }
